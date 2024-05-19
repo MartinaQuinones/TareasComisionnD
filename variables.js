b9f795195e3216c8eaf84f33f5f69cca6196d9df
MartinaQuinones <166770009+MartinaQuinones@users.noreply.github.com>
2024-05-18 22:18:22 -0300

    Add files via upload
    Tarea numero 10

---
+++ variables.js
@@ -0,0 +1,11 @@
+var nombre="Martina ";
+var apellido="Quiñones";
+
+console.log(nombre + apellido);
+
+var cantidad= 20
+var cantidadd= 30
+var suma= (cantidad + cantidadd)
+var massuma= (suma + 50)
+
+console.log(massuma)
